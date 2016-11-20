0275fc5b950a1ab944c6a8cdc93303e07f80b1e8
2757777 <ikarosuchihaya@gmail.com>
2016-11-20 13:00:57 +0900

    show postion

---
+++ Assets/GetWorldPostion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GetWorldPostion : MonoBehaviour
+{
+
+    public Text PostionText;
+    public GoMap.LocationManager Location;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PostionText.text = "latitude=" + Location.currentLocation.latitude + "\nlongitude=" + Location.currentLocation.longitude;
+    }
+}
