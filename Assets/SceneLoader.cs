40a2fc4a4de0b36c0f1d86cacc3421d2a9247f9f
2757777 <ikarosuchihaya@gmail.com>
2016-11-20 14:30:11 +0900

    add scan button  & make scene link

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour {
+    public string SceneName;
+	// Use this for initialization
+	void Start () 
+    {
+      Button  button = GetComponent<Button>();
+        button.onClick.AddListener(ShowScene);
+
+	}
+	
+	// Update is called once per frame
+    void ShowScene()
+    {
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Additive);
+	}
+}
