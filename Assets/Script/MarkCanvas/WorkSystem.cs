92ff24e4ac27cb2f90d3db4df2b61751eddc3743
Li Fengsheng <fengsheng@mfac.jp>
2016-12-06 18:59:03 +0900

    make informationsystem

--- Assets/Script/MarkCanvas/WorkSystem.cs
+++ Assets/Script/MarkCanvas/WorkSystem.cs
@@ -15,6 +15,13 @@ public class WorkSystem : MonoBehaviour {
 
     public Canvas MarkDetailCanvas;
 
+
+    public GameObject KnowledgeBar;
+    public GameObject CourageBar;
+    public GameObject DexterityBar;
+    public GameObject GentlenessBar;
+    public GameObject CharmBar;
+
     void Start () 
     {
         Button button = GetComponent<Button>();
@@ -31,7 +38,30 @@ public class WorkSystem : MonoBehaviour {
             MoneyText.text = "Money:" + status.HaveMoney.ToString();
             status.SaveMoney();
             MarkDetailCanvas.enabled = false;
+            PulsParameter();
         }
 
 	}
+
+    void PulsParameter()
+    {
+        switch (Random.Range(0, 5))
+        {
+            case 0:
+                KnowledgeBar.GetComponent<EnergyBar>().valueCurrent++;
+                break;
+            case 1:
+                CourageBar.GetComponent<EnergyBar>().valueCurrent++;
+                break;
+            case 2:
+                DexterityBar.GetComponent<EnergyBar>().valueCurrent++;
+                break;
+            case 3:
+                GentlenessBar.GetComponent<EnergyBar>().valueCurrent++;
+                break;
+            case 4:
+                CharmBar.GetComponent<EnergyBar>().valueCurrent++;
+                break;
+        }
+    }
 }
---
+++ Assets/Script/information/InformationSystem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationSystem : MonoBehaviour
+{
+
+    public GameObject InformationText;
+
+    public GameObject Parentcanvans;
+
+    private string information;
+
+   public void informationStandby (string infor)
+   {
+       information = infor;
+	    Invoke("DelayMethod", 3.5f);
+
+	}
+
+    void DelayMethod()
+    {
+        GameObject informationObj =  Instantiate(InformationText);
+
+        informationObj.GetComponent<Text>().text = information;
+        informationObj.transform.SetParent(Parentcanvans.transform);
+
+        Destroy(informationObj,3f);
+    }
+
+}
