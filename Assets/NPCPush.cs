72e38475235c378c46171f3ad7a562ac07ad45f2
2757777 <ikarosuchihaya@gmail.com>
2016-12-10 17:12:04 +0900

    add NPC

---
+++ Assets/NPCPush.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCPush : MonoBehaviour
+{
+    public GameObject[] NPC;
+    public Transform NPCParent;
+    void Start()
+    {
+        NPCParent = this.gameObject.transform;
+        if (Random.Range(0, 10) == 0)
+        {
+            InstantiateNPC();
+        }
+    }
+
+    void InstantiateNPC()
+    {
+        int NPCNumber= Random.Range(0, NPC.Length);
+        GameObject NPCObject = Instantiate(NPC[NPCNumber]);
+        NPCObject.transform.position = new Vector3((NPCParent.position.x + Random.Range(10, 20)), 2, (NPCParent.position.z + Random.Range(10, 20)));
+        NPCObject.transform.localScale = new Vector3(8, 8, 8);
+        Animator animator = NPCObject.GetComponent<Animator>();
+        animator.SetBool("IsWalk", false);
+        animator.SetBool("Set", true);
+    }
+
+}
